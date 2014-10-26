88ba9a8ca27a2dcae82bb63b234d1e247bcb4240
Bacou, Melanie (buster) <mel@mbacou.com>
2014-10-25 21:27:27 -0600

    added web app

--- inst/www/example.js
+++ inst/www/example.js
@@ -2,15 +2,31 @@
 //ocpu.seturl("//harvestchoice.ocpu.io/hcapi3/R")
 
 //call R function: stocks::smoothplot(ticker=ticker)
-$("#submitbutton").click(function(){
+$("#submitbutton").click(function () {
     var layer = $("#layer").val();
     var req = $("#plotdiv").rplot("genPlot", {
-        var : layer
+        "var": layer
     });
 
     //optional
-    req.fail(function(){
+    req.fail(function () {
         alert("R returned an error: " + req.responseText);
     });
 });
+
+//call R function: tvscore::tv(input=data)
+$("#aggbutton").click(function () {
+    var layer = $("#layer").val();
+    var by = $("#by").val();
+
+    var req = ocpu.rpc("getLayer", {
+        "var": layer,
+        "by": by
+    }, function (output) {
+        $("tbody").empty();
+        $.each(output, function (index, value) {
+            var html = "<tr><td>" + value.ISO3 + "</td><td>" + value.whea_p + "</td></tr>";
+            $("tbody").append(html);
+        });
+    });
 
