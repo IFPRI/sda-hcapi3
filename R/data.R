f6c015baffc55528364f46cdb2c87fb05e8d6860
Bacou, Melanie (buster) <mel@mbacou.com>
2014-11-03 06:04:22 -0500

    Added separate data package hcdata

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Country Boundaries
+#'
+#' GAUL 2008 country boundaries for sub-Saharan Africa used in HarvestChoice data layers.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name g0
+#' @usage data(g0)
+#' @format A SpatialPolygonsDataFrame with 56 obs. and 12 variables
+NULL
+
+#' Province Boundaries
+#'
+#' GAUL 2008 province boundaries for sub-Saharan Africa used in HarvestChoice data layers.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name g1
+#' @usage data(g1)
+#' @format A SpatialPolygonsDataFrame with 3,240 obs. and 15 variables
+NULL
