154a08b5c967d5cfeb0eec1c2278fc412eaf2b26
Bacou, Melanie (buster) <mel@mbacou.com>
2017-05-19 04:09:09 -0600

    enforce data.table unique() old behavior

--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,6 @@
 .onLoad <- function(libname, pkgname) {
   # Set remote Rserve server and port
   options(na.last=T, hcapi3.host="hcapi.harvestchoice.org", hcapi3.port=9100L)
+  # data.table unique() and duplicated() new behavior can break code
+  options(datatable.old.unique.by.key=TRUE)
 }
